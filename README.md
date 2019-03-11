1d37c987f25f824cfab81ade8fc7ca6d5528c1a4
apizarras <angelalee.p@gmail.com>
2019-03-11 11:35:41 -0500

    js changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # donutsNomNomNom
+
+3.6.19 wireframe,html and css
+
+3.10.19 psuedo code javascript
---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+//instructions to being the game
+
+
+//define winning array values
+const donuts = ["jelly", "crueller", "sugar","glazed", "buttermilk", "cake", "fritter", "cronut", "eclair"];
+
+//randomly chosing a value from the donut array
+var randomDonut = donuts[Math.floor(Math.random() * donuts.length)];
+console.log(randomDonut);
+//set the number of guesses the player can have
+var numGuessesLeft = 15;
+document.getElementById("num-Guesses-Left").innerHTML = numGuessesLeft;
+var resetGuesses = " ";
+
+//alert("Are you ready to start the game?");
+//count the number of times the user has guessed the word correctly
+
+//declare variables for wins and losses
+var wins = 0;
+var losses = 0;
+var userLetterGuess = document.getElementById("userLetterGuess");
+var numWins = document.getElementById("wins");
+var numLosses = document.getElementById("losses");
+var wrandomDonut = document.getElementById("word-to-use");
+var currentWord = [];
+
+console.log (randomDonut.length);
+//looping through the current word and counting the number of letters in the word to show blanks for each letter
+for (i=0; i<randomDonut.length; i++) {
+    currentWord.push("_");
+}    
+document.getElementById("word-To-Use").innerHTML = currentWord.join(" ");
+
+//recognize and store value of user input
+var userLetter = [];
+document.onkeyup = function(event) {
+    var userLetter = event.key;
+}
+//compare to randomly generated word and identify where that letter falls in the word
+for (var j = 0; j <randomDonut.length; j++) {
+    if (randomDonut[j] === userLetter) {
+        userLetter.push[j];
+    }
+}
+
+//display matching letter
+
+//deal with letter that is guessed incorrectly
+
+//determine if user won or lost
+
+//update stats on user wins and losses

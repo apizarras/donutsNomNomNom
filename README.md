62160ae8a37361d39f404b368c779b8c6b744234
apizarras <angelalee.p@gmail.com>
2019-03-13 18:35:33 -0500

    git@github.com:apizarras/donutsNomNomNom.git

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 3.6.19 wireframe,html and css
 
 3.10.19 psuedo code javascript
+
+3.13.19 fixed you've own message to appear
--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -5,14 +5,17 @@ const donuts = ["cake", "sugar", "cronut", "eclair", "jelly", "fritter", "cruell
 var numTries = 10;
 var wins=0;
 var guessedLetters=[];
-blanksForLetters = [];
+var blanksForLetters = [];
+var correctGuesses=0;
+
 //start game, commented out for testing functions
 function start() {
-
+wordComplete();
 //pick a random donut from the donuts array
 var randomDonut = donuts[Math.floor(Math.random() * donuts.length)];
 console.log(randomDonut);
-console.log(randomDonut.length);
+var randomDonutLength = randomDonut.length;
+console.log(randomDonutLength);
 
 //display the  number of blanks for the current random donut word
 var blanksForLetters = [];
@@ -36,27 +39,39 @@ document.onkeyup = function (event) {
     var guess = event.key;
 console.log(guess);
 //determine what position within the current randomDonut word the letter is located and then display that
-numTries--;
+
    if(randomDonut.indexOf(guess) == -1 && numTries>0) {
         guessedLetters.push(guess);
         document.getElementById("lettersGuessed").innerHTML = guessedLetters;
         console.log(guessedLetters);
+        numTries--;
         console.log(numTries);
         document.getElementById("num-Guesses-Left").innerHTL = numTries;
         restart();
    }
+
    if (randomDonut.includes(guess) && numTries>0) {
        var index = randomDonut.indexOf(guess);
        blanksForLetters.splice(index, 1, guess);
        document.getElementById("word-To-Use").innerHTML=blanksForLetters.join(" ");
-
+        numTries--;
+        correctGuesses++;
+        console.log(correctGuesses);
+        wordComplete();
    }
 };
 
+function wordComplete () {
+    console.log(randomDonutLength);
+    if (randomDonutLength == correctGuesses) {
+    wins++;
+    document.getElementById("you-won").innerHTML = "<h2>You've Won!!!</h1>";
+    }
+}
 function restart() {
     if(numTries <=0) {
         wins++;
-        document.getElementById("you-won").innerHTML = "You've Won!!!";
+        document.getElementById("you-won").innerHTML.textContent = "You've Won!!!";
     document.getElementById("wins").innerHTML = wins;
     start();
     } else if (numTries < 0) {
